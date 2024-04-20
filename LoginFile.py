c91fbaeb2d71989233bc1c69b49de01b8cc6bb3b
Xandre-Nicolas <xamnicolas@mymail.mapua.edu.ph>
2024-04-20 15:34:24 +0800

    defined the function of the login()

--- LoginFile.py
+++ LoginFile.py
@@ -8,3 +8,12 @@ window.title("Login form")
 window.geometry('500x500')
 window.configure(bg='#333333')
 
+def login():
+    username = "Group6"
+    password = "lesgo"
+    if username_entry.get()==username and password_entry.get()==password:
+        messagebox.showinfo(title="Login Success", message="You successfully logged in.")
+    else:
+        messagebox.showerror(title="Error", message="Invalid login.")
+
+frame = tkinter.Frame(bg='#333333')
